10ef8b34bc967f70d416b30fe63e8fad927902f8
Anokhin <a.i.anohin.arduino@gmail.com>
2024-07-10 16:47:47 +0300

    UPD testing

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,6 +3,8 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,26 @@ import java.util.List;
 public class CarsController {
 
     @GetMapping(value = "/cars")
-    public String printCars(Model model) {
+    public String printCars(@RequestParam(value = "marka", required = false) String marka,
+                            @RequestParam(value = "type", required = false) String type,
+                            Model model) {
+        model.addAttribute("carsParamerts", "Marka: " + marka + ", type: " + type);
         return "cars";
+    }
+
+    @GetMapping("/calc")
+    public String printCalc(@RequestParam("a") double a,
+                            @RequestParam("b") double b,
+                            @RequestParam("action") String action,
+                            Model model) {
+        double result = 0.0;
+        switch (action){
+            case "multiply": result = a*b; break;
+            case "division": result = a/b; break;
+            case "addition": result = a+b; break;
+            case "substraction": result=a-b; break;
+        }
+        model.addAttribute("result", "Result: " + result);
+        return "calc";
     }
 }
